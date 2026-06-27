b64c53cb169c71d6044c69882a90aa0f05a4e67f
BAL_Vibe_ai <balpoonuk@gmail.com>
2026-06-27 01:30:14 +0100

    Auto-update student portal indexes and grades

--- ho_dyun/data_memo.js
+++ ho_dyun/data_memo.js
@@ -1,6 +1,11 @@
 // Automatically generated memo data
 const studentMemo = [
   {
+    "type": "功課",
+    "content": "1. 《高空擲物》短文\n2. 練習講故事",
+    "due": "2026-07-01"
+  },
+  {
     "type": "溫習目標",
     "content": "溫習“看圖說故事” 7個必做元素，以及需要牢記要找到圖中的主要角色，構思故事與人物互動",
     "due": "2026-06-26"
