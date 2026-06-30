bd2329726f7884b65ac7c285dc1b757e82bf2ac5
BAL_Vibe_ai <balpoonuk@gmail.com>
2026-06-30 11:39:44 +0100

    Auto-update student portal indexes and grades

--- pak_ching/data_memo.js
+++ pak_ching/data_memo.js
@@ -1,6 +1,11 @@
 // Automatically generated memo data
 const studentMemo = [
   {
+    "type": "功課",
+    "content": "完成作文《舊車票》",
+    "due": "2026-07-07"
+  },
+  {
     "type": "其他",
     "content": "下堂補回15分鐘",
     "due": "2026-07-07"
