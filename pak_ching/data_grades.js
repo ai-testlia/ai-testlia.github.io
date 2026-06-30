49d1ff84f1ebe4b0f60a80c5c541fe084834c670
BAL_Vibe_ai <balpoonuk@gmail.com>
2026-06-30 11:36:40 +0100

    Auto-update student portal indexes and grades

--- pak_ching/data_grades.js
+++ pak_ching/data_grades.js
@@ -1,2 +1,17 @@
 // Automatically generated grades data
-const studentGrades = [];
+const studentGrades = [
+  {
+    "date": "2026-06-30",
+    "test_name": "S3 Final 卷一",
+    "score": 82.0,
+    "max_score": 100.0,
+    "remarks": "文言文閱讀理解看得明白但不知怎樣寫"
+  },
+  {
+    "date": "2026-06-30",
+    "test_name": "S3 Final 卷二",
+    "score": 69.0,
+    "max_score": 100.0,
+    "remarks": "全級只有幾個人能拿7字頭"
+  }
+];
--- pak_ching/data_memo.js
+++ pak_ching/data_memo.js
@@ -1,2 +1,8 @@
 // Automatically generated memo data
-const studentMemo = [];
+const studentMemo = [
+  {
+    "type": "其他",
+    "content": "下堂補回15分鐘",
+    "due": "2026-07-07"
+  }
+];
