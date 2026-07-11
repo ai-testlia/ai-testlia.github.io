af7492d0e6ac78f5ba0b1960c47c186b86d9e567
BAL_Vibe_ai <balpoonuk@gmail.com>
2026-07-11 04:04:21 +0100

    Auto-update student portal indexes and grades

--- Himson/data_memo.js
+++ Himson/data_memo.js
@@ -1,2 +1,8 @@
 // Automatically generated memo data
-const studentMemo = [];
+const studentMemo = [
+  {
+    "type": "功課",
+    "content": "《原來我沒有想像中勇敢》作文",
+    "due": "2026-07-15"
+  }
+];
--- Yat_Hong/data_memo.js
+++ Yat_Hong/data_memo.js
@@ -1,6 +1,11 @@
 // Automatically generated memo data
 const studentMemo = [
   {
+    "type": "功課",
+    "content": "《舊車票》作文 - 下星期一交",
+    "due": "2026-07-13"
+  },
+  {
     "type": "溫習目標",
     "content": "暑期目標︰以作文和文化知識為主，結合深度閱讀和討論，增加知識量及改善寫作思維",
     "due": "2026-08-27"
