179b76d371580f01b91ba7f0b7b86338175a8a9a
BAL_Vibe_ai <balpoonuk@gmail.com>
2026-09-16 12:35:51 +0100

    Auto-update student portal indexes and grades

--- Hei_yu/data_memo.js
+++ Hei_yu/data_memo.js
@@ -1,2 +1,8 @@
 // Automatically generated memo data
-const studentMemo = [];
+const studentMemo = [
+  {
+    "type": "功課",
+    "content": "1.完成【一詞多義】練習第2部份 (熱身)\n2. 閱讀及預習【一詞多義】第三部份",
+    "due": "2026-09-21"
+  }
+];
